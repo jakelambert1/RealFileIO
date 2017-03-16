2551d1147dd158e5b718109bb665a9435ee717fe
Jake Lambert <Jake Lambert>
2017-03-16 10:58:45 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a0lambj41/realfileio/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.a0lambj41.realfileio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.save)
+        {
+            // react to the menu item being selected...
+            return true;
+        }
+        return false;
+    }
+}
